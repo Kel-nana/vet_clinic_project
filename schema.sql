c166bd476cd345b586318e90113726879c85f5eb
kelvinirungu <kelvinn.irungu69@gmail.com>
2023-07-10 13:53:35 +0300

    Addition of table schema, table data and queries

--- schema.sql
+++ schema.sql
@@ -1,5 +1,8 @@
 /* Database schema to keep the structure of entire database. */
 
 CREATE TABLE animals (
-    name varchar(100)
+    id SERIAL PRIMARY KEY,
+    name varchar(50), date_of_birth date,
+    escape_attempts INT, neutered boolean,
+    weight_kg decimal
 );
