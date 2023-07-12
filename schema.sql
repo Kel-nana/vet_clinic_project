30c31c7eeeda5f931ece400d6b2a70a83cdf6c57
kelvinirungu <kelvinn.irungu69@gmail.com>
2023-07-12 12:23:49 +0300

    Addition of owners, species and alteration of animals table

--- schema.sql
+++ schema.sql
@@ -8,3 +8,21 @@ CREATE TABLE animals (
 );
 ALTER TABLE animals
 ADD COLUMN species varchar(20);
+
+CREATE TABLE owners (
+  id int GENERATED ALWAYS AS IDENTITY,
+  full_name varchar,
+  age int,
+  PRIMARY KEY(id)
+);
+
+CREATE TABLE species (
+  id int GENERATED ALWAYS AS IDENTITY,
+  name varchar,
+  PRIMARY KEY(id)
+);
+
+ALTER TABLE animals
+  DROP COLUMN species;
+ALTER TABLE animals ADD species_id int REFERENCES species(id);
+ALTER TABLE animals ADD owner_id int REFERENCES owners(id);
