09bbbba51a66f73087fa413345d3b608297ce657
kelvinirungu <kelvinn.irungu69@gmail.com>
2023-07-13 13:20:28 +0300

    Addition of vets, specialization and visits table

--- schema.sql
+++ schema.sql
@@ -26,3 +26,29 @@ ALTER TABLE animals
   DROP COLUMN species;
 ALTER TABLE animals ADD species_id int REFERENCES species(id);
 ALTER TABLE animals ADD owner_id int REFERENCES owners(id);
+
+CREATE TABLE vets(
+    id SERIAL PRIMARY KEY,
+    name varchar,
+    age int,
+    date_of_graduation date
+);
+
+CREATE TABLE vets(
+    id SERIAL PRIMARY KEY,
+    name varchar,
+    age int,
+    date_of_graduation date
+);
+
+CREATE TABLE specializations (
+  vet_id int REFERENCES vets(id),
+  species_id int REFERENCES species(id)
+);
+
+CREATE TABLE visits (
+  animal_id int REFERENCES animals(id),
+  date_of_visit date,
+  vet_id int REFERENCES vets(id)
+);
+
